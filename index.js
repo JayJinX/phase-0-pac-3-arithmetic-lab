27c01e649409bfa962a083ff5dccdccb394085d8
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-05 09:21:26 +1100

    Done with assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    return num1 += num2;
+}
+
+function subtract(num1, num2) {
+    return num1 -= num2;
+}
+
+function divide(num1, num2) {
+    return num1 /= num2;
+}
+
+function multiply(num1, num2) {
+    return num1 *= num2;
+}
+
+function increment(number) {
+    return ++ number;
+}
+
+function decrement(number) {
+    return --number;
+}
+
+function makeInt(number) {
+    return parseInt(number, 10)
+}
+
+function preserveDecimal(number) {
+    return parseFloat(number);
+}
